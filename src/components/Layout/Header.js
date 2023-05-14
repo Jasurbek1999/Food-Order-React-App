d3aaf1cb3b67945be42b2018315f7fc3166158a6
Jasurbek1999 <jasurbekabdurakhmonov79@gmail.com>
2023-05-14 21:07:04 +0900

    overall food app project commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton onClick={props.onShowCart}/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A table of food!" />
+        </div>
+    </Fragment>
+}
+
+export default Header;
